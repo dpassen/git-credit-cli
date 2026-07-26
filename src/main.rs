7679184f1facd775d465ad26302c76bf8be845cf
Derek Passen <dpassen1@gmail.com>
2026-07-26 13:43:31 -0500

    Drop the unnecessary Head wrapper

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ fn main() -> anyhow::Result<()> {
     Args::parse();
 
     let cwd = std::env::current_dir()?;
-    let head = git::inspect_head(&cwd)?;
-    let contributors = git::discover_contributors(&cwd, &head)?;
+    let head_oid = git::inspect_head(&cwd)?;
+    let contributors = git::discover_contributors(&cwd, &head_oid)?;
 
     if contributors.is_empty() {
         anyhow::bail!("no usable contributors found");
